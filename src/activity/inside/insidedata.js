2ebe6861bbca8ea86ba1e179221d54dc8c54d430
Jason <jasonalvarezcoraiza@gmail.com>
2020-04-18 18:15:01 -0400

    added content to the info tabs

--- src/activity/inside/insidedata.js
+++ src/activity/inside/insidedata.js
@@ -1,3 +1,5 @@
+import NewYorkTimesLogo from "../../Icons/NewYorkTimesLogo.png";
+
 class Projects {
 
   constructor(id, parents, info,link,image) {
@@ -15,10 +17,67 @@ class Projects {
   }
 }
 
-let insideactivitys = [
-   new Projects("testing",["Aldair"],"ho there","https://agarciany.github.io/piglaten/","https://i.imgur.com/CScNoZw.pngs"),
 
-];
+
+async function content(){
+  
+  //most viewed new york times articles
+
+  fetch("https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=lI4WHMHoiOnZHoJwYyLmqAHuHs4IZZN8")
+  .then((data)=>{
+      return data.json();
+  })
+  .then((response)=>{
+    for(let i=0; i<response.results.length;i++){
+          insideactivitys.push(new Projects(response.results[i].title, ["Jason"], response.results[i].abstract, response.results[i].url, NewYorkTimesLogo));
+      }
+      return response.results;
+  });
+
+  //books to read topsellers
+
+  fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=lI4WHMHoiOnZHoJwYyLmqAHuHs4IZZN8")
+  .then((data)=>{
+      return data.json();
+  })
+  .then((response)=>{
+      // console.log("books to read",response.results.books);
+      for(let i=0; i<response.results.books.length;i++){
+          insideactivitys.push(new Projects(response.results.books[i].title, ["Jason"], response.results.books[i].description, response.results.books[i].url, response.results.books[i].book_image));
+      }
+      return response.results.books;
+  })
+
+
+  //top stories about home nyt
+
+  fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=lI4WHMHoiOnZHoJwYyLmqAHuHs4IZZN8")
+  .then((data)=>{
+      return data.json();
+  })
+  .then((response)=>{
+      // console.log("articles to read",response.results);
+      for(let i=0; i<response.results.length;i++){
+        insideactivitys.push(new Projects(response.results[i].title, ["Jason"], response.results[i].abstract, response.results[i].url, response.results[i].multimedia[0].url));
+      }
+      return response.results;
+  });
+
+  // fetch("https://raw.githubusercontent.com/soroushchehresa/450-free-courses/master/README.md")
+  // .then((data)=>{
+  //     // console.log("free courses", data);
+  //     return data.text();
+  // })
+  // .then((response)=>{
+  //     let spiltData = response.split("<h2");            
+  //     // console.log(spiltData);
+  // });
+
+}
+content();
+
+
+let insideactivitys = [];
 console.log(insideactivitys);
 export default insideactivitys;
   
